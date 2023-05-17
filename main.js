e3d1e868cae9628fd0efdb53d62c561f09cf47e4
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-17 12:28:01 +0300

    fact mental swing plural mud

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const navItems = document.querySelector(".nav_items");
-const openNavBtn = document.querySelector("#open_nav-btn");
-const closeNavBtn = document.querySelector("#close_nav-btn");
+const navItems = document.querySelector('.nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
 
 const openNav = () => {
     navItems.Style.display = 'flex';
