2a0bfdd03fafc28e07d937a85d38f79b0e4b6333
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-17 13:02:01 +0300

    provide making national swept receive

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const navItems = document.querySelector('.nav_items');
 const openNavBtn = document.querySelector('#open_nav-btn');
 const closeNavBtn = document.querySelector('#close_nav-btn');
 
+// opens nav dropdown
+const openNav = () => {
+    navItems.Style.display = 'flex';
+    openNavBtn.Style.display = 'none';
+    openNavBtn.Style.display = 'inline-block';
+}
+
+// close nav dropdown
 const openNav = () => {
     navItems.Style.display = 'flex';
     openNavBtn.Style.display = 'none';
