70a775fb45a4f79797c134dd2b3ba852db0bb191
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-17 13:04:01 +0300

    air bigger wonderful blood involved

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const closeNavBtn = document.querySelector('#close_nav-btn');
 const openNav = () => {
     navItems.Style.display = 'flex';
     openNavBtn.Style.display = 'none';
-    openNavBtn.Style.display = 'inline-block';
+    closeNavBtn.Style.display = 'inline-block';
 }
 
 // close nav dropdown
-const openNav = () => {
-    navItems.Style.display = 'flex';
-    openNavBtn.Style.display = 'none';
+const closeNav = () => {
+    navItems.Style.display = 'none';
     openNavBtn.Style.display = 'inline-block';
+    closeNavBtn.Style.display = 'none';
 }
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
