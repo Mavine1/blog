ed125718364fd048b508a468ea2fc09605b756c3
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-06-18 00:52:00 +0300

    street wonder belt station explain

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const navItems = document.querySelector('.nav_items');
+const navContainer = document.querySelector('.nav_items');
 const openNavBtn = document.getElementById('open_nav-btn');
 const closeNavBtn = document.getElementById('close_nav-btn');
 
 const openNav = () => {
-  navItems.style.display = 'flex';
+  navContainer.style.display = 'flex';
   openNavBtn.style.display = 'none';
   closeNavBtn.style.display = 'inline-block';
 };
 
 const closeNav = () => {
-  navItems.style.display = 'none';
+  navContainer.style.display = 'none';
   openNavBtn.style.display = 'inline-block';
   closeNavBtn.style.display = 'none';
 };
